d0b6ff5567b0d48324be3c2bdb0bf94df5911d4f
EdenBoy03 <153565904+EdenBoy03@users.noreply.github.com>
2025-11-02 13:20:29 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Course from "./pages/Course";
 import Inbox from "./pages/Inbox";
+import Account from "./pages/Account";
 // import Assignment from "./pages/Assignment";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Route path="/" element={<Dashboard />} />
         <Route path="/course/:courseId" element={<Course />} />
         <Route path="/inbox" element={<Inbox />} />
-
+        <Route path="/account" element={<Account />} />
         {/* <Route path="/course/:courseId/assignment/:assignmentId" element={<Assignment />} /> */}
       </Routes>
     </Router>
